c08b519a8ddde84c10057b938ae17b76c226572e
timofeymukha <timofey.mukha@it.uu.se>
2017-08-07 10:55:48 +0200

    Added Write.jl

--- Blockster.jl
+++ Blockster.jl
@@ -10,6 +10,7 @@ include("MeshPrimitives.jl")
 include("Edges.jl")
 include("Blocks.jl")
 include("MeshCreate.jl")
+include("Write.jl")
 
 #using .MeshCreate
 #using .MeshPrimitives
@@ -84,18 +85,6 @@ end
 
 
 
-
-function write_header(file, class, location, object)
-    write(file, "FoamFile\n")
-    write(file, "{\n")
-    write(file, "    version     2.0;\n")
-    write(file, "    format      ascii;\n")
-    write(file, "    class       $class;\n")
-    write(file, "    location    \"$location\";\n")
-    write(file, "    object      $object;\n")
-    write(file, "}\n\n")
-end
-
 function create_blocks(dict, vertices)
     nBlocks = size(dict["blocks"], 1)
     blocks = Vector{Block}(nBlocks)
@@ -131,31 +120,6 @@ function create_blocks(dict, vertices)
     return blocks
 end
 
-function write_mesh_information(
-    nPoints,
-    nCells,
-    nFaces,
-    nInternalFaces,
-    patchNames,
-    patchStarts,
-    patchSizes
-)
-    println("----------------")
-    println("Mesh Information")
-    println("----------------")
-    println("  nPoints $(nPoints)")
-    println("  nCells $(nCells)")
-    println("  nFaces $(nFaces)")
-    println("  nInternalFaces $(nInternalFaces)")
-
-    println("----------------")
-    println("Patches")
-    println("----------------")
-
-    for i in 1:length(patchSizes)
-        println("  patch $i (start: $(patchStarts[i]) size: $(patchSizes[i])) name: $(patchNames[i])")
-    end
-end
 
 
 function parse_vertices(varsAsStr::Vector{String}, vertices)
@@ -295,62 +259,17 @@ function main(args)
        mkpath(joinpath(".", "test_case", "constant", "polyMesh"))
     end
 
-    ownerFile = open(joinpath(".", "test_case", "constant", "polyMesh", "owner"), "w")
-    write_header(ownerFile, "labelList", "constant/polyMesh", "owner")
-    write(ownerFile, "$(size(owner, 1))\n") 
-    write(ownerFile, "(\n")
-    for i in 1:size(owner, 1)
-        write(ownerFile, "$(owner[i])\n")
-    end
-    write(ownerFile, ")\n")
-    close(ownerFile)
-
-    neighbourFile = open(joinpath(".", "test_case", "constant", "polyMesh", "neighbour"), "w")
-    write_header(neighbourFile, "labelList", "constant/polyMesh", "neighbour")
-    write(neighbourFile, "$(size(neighbour, 1))\n") 
-    write(neighbourFile, "(\n")
-    for i in 1:size(neighbour, 1)
-        write(neighbourFile, "$(neighbour[i])\n")
-    end
-    write(neighbourFile, ")\n")
-    close(neighbourFile)
-
-    pointsFile = open(joinpath(".", "test_case", "constant", "polyMesh", "points"), "w")
-    write_header(pointsFile, "vectorField", "constant/polyMesh", "points")
-    write(pointsFile, "$(size(points, 1))\n") 
-    write(pointsFile, "(\n")
-    for i in 1:size(points, 1)
-        write(pointsFile, "($(points[i][1]) $(points[i][2]) $(points[i][3]))\n")
-    end
-    write(pointsFile, ")\n")
-    close(pointsFile)
-
-    facesFile = open(joinpath(".", "test_case", "constant", "polyMesh", "faces"), "w")
-    write_header(facesFile, "faceList", "constant/polyMesh", "faces")
-    write(facesFile, "$(size(faces, 1))\n") 
-    write(facesFile, "(\n")
-    for i in 1:size(faces, 1)
-        write(facesFile, "4($(faces[i][1]) $(faces[i][2]) $(faces[i][3]) $(faces[i][4]))\n")
-    end
-    write(facesFile, ")\n")
-    close(facesFile)
-
-    boundaryFile = open(joinpath(".", "test_case", "constant", "polyMesh", "boundary"), "w")
-    write_header(boundaryFile, "polyBoundaryMesh", "constant/polyMesh", "boundary")
-    write(boundaryFile, "$(size(patches, 1))\n") 
-    write(boundaryFile, "(\n")
-    for i in 1:size(patches, 1)
-        write(boundaryFile, "    $(patchNames[i])\n")
-        write(boundaryFile, "    {\n")
-        write(boundaryFile, "    type      wall;\n")
-#        write(boundaryFile, "    inGroups\n")
-        write(boundaryFile, "    nFaces    $(patchSizes[i]);\n")
-        write(boundaryFile, "    startFace $(patchStarts[i] - 1);\n")
-        write(boundaryFile, "    }\n")
-    end
-
-    write(boundaryFile, ")\n")
-    close(boundaryFile)
+    writeDir = joinpath(".", "test_case", "constant", "polyMesh")
+    write_mesh(
+        writeDir,
+        owner,
+        neighbour,
+        points,
+        faces,
+        patchNames,
+        patchStarts,
+        patchSizes
+    )
 
     write_mesh_information(
         nPoints,
---
+++ Write.jl
@@ -0,0 +1,108 @@
+export write_header, write_mesh_information, write_mesh
+
+function write_header(file, class, location, object)
+    write(file, "FoamFile\n")
+    write(file, "{\n")
+    write(file, "    version     2.0;\n")
+    write(file, "    format      ascii;\n")
+    write(file, "    class       $class;\n")
+    write(file, "    location    \"$location\";\n")
+    write(file, "    object      $object;\n")
+    write(file, "}\n\n")
+end
+
+function write_mesh_information(
+    nPoints,
+    nCells,
+    nFaces,
+    nInternalFaces,
+    patchNames,
+    patchStarts,
+    patchSizes
+)
+    println("----------------")
+    println("Mesh Information")
+    println("----------------")
+    println("  nPoints $(nPoints)")
+    println("  nCells $(nCells)")
+    println("  nFaces $(nFaces)")
+    println("  nInternalFaces $(nInternalFaces)")
+
+    println("----------------")
+    println("Patches")
+    println("----------------")
+
+    for i in 1:length(patchSizes)
+        println("  patch $i (start: $(patchStarts[i]) size: $(patchSizes[i])) name: $(patchNames[i])")
+    end
+end
+
+function write_mesh(
+    dir,
+    owner,
+    neighbour,
+    points,
+    faces,
+    patchNames,
+    patchStarts,
+    patchSizes
+)
+    ownerFile = open(joinpath(dir, "owner"), "w")
+    neighbourFile = open(joinpath(dir, "neighbour"), "w")
+    pointsFile = open(joinpath(dir, "points"), "w")
+    facesFile = open(joinpath(dir, "faces"), "w")
+    boundaryFile = open(joinpath(dir, "boundary"), "w")
+
+    write_header(ownerFile, "labelList", "constant/polyMesh", "owner")
+    write(ownerFile, "$(size(owner, 1))\n") 
+    write(ownerFile, "(\n")
+    for i in 1:size(owner, 1)
+        write(ownerFile, "$(owner[i])\n")
+    end
+    write(ownerFile, ")\n")
+    close(ownerFile)
+
+    write_header(neighbourFile, "labelList", "constant/polyMesh", "neighbour")
+    write(neighbourFile, "$(size(neighbour, 1))\n") 
+    write(neighbourFile, "(\n")
+    for i in 1:size(neighbour, 1)
+        write(neighbourFile, "$(neighbour[i])\n")
+    end
+    write(neighbourFile, ")\n")
+    close(neighbourFile)
+
+    write_header(pointsFile, "vectorField", "constant/polyMesh", "points")
+    write(pointsFile, "$(size(points, 1))\n") 
+    write(pointsFile, "(\n")
+    for i in 1:size(points, 1)
+        write(pointsFile, "($(points[i][1]) $(points[i][2]) $(points[i][3]))\n")
+    end
+    write(pointsFile, ")\n")
+    close(pointsFile)
+
+    write_header(facesFile, "faceList", "constant/polyMesh", "faces")
+    write(facesFile, "$(size(faces, 1))\n") 
+    write(facesFile, "(\n")
+    for i in 1:size(faces, 1)
+        write(facesFile, "4($(faces[i][1]) $(faces[i][2]) $(faces[i][3]) $(faces[i][4]))\n")
+    end
+    write(facesFile, ")\n")
+   close(facesFile)
+
+    nPatches = length(patchNames)
+    write_header(boundaryFile, "polyBoundaryMesh", "constant/polyMesh", "boundary")
+    write(boundaryFile, "$(nPatches))\n") 
+    write(boundaryFile, "(\n")
+    for i in 1:nPatches
+        write(boundaryFile, "    $(patchNames[i])\n")
+        write(boundaryFile, "    {\n")
+        write(boundaryFile, "    type      wall;\n")
+        write(boundaryFile, "    inGroups\n")
+        write(boundaryFile, "    nFaces    $(patchSizes[i]);\n")
+        write(boundaryFile, "    startFace $(patchStarts[i] - 1);\n")
+        write(boundaryFile, "    }\n")
+    end
+
+    write(boundaryFile, ")\n")
+    close(boundaryFile)
+end
